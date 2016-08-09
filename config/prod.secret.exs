5ace6ba925f7d3a1bbe3ba2fb06f78d17dd3567c
Smith Aitufe <smithaitufe@live.com>
2016-08-09 03:30:00 +0100

    Added Database Url to prod.secret.exs

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -4,12 +4,10 @@ use Mix.Config
 # you likely want to automate and keep it away from
 # your version control system.
 config :store, Store.Endpoint,
-  secret_key_base: "xGLRgSfiXwNspmFRrFhsyaf0IW3S7Sll0+pNtkis6jepXwEhriqGVcUyL0Fq6ryS"
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Configure your database
 config :store, Store.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
-  database: "store_prod",
+  url: System.get_env("DATABASE_URL")
   pool_size: 20
