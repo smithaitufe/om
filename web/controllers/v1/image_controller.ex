06ff3c8073adcfd22a41d92fa9019e9cf9bb2fcc
Samuel Smith <smithaitufe@live.com>
2017-01-14 23:21:00 +0100

    Generated arc file uploader. Modified image controller to include scrub for create and update params

--- web/controllers/v1/image_controller.ex
+++ web/controllers/v1/image_controller.ex
@@ -1,6 +1,6 @@
 defmodule Store.V1.ImageController do
     use Store.Web, :controller
-    # plug :scrub_params, "image" when action in [:create, :update]
+    plug :scrub_params, "image" when action in [:create, :update]
 
     def index(conn, _params) do
         
